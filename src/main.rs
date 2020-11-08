eb4400198934a2ed42e89d30848744d7cffba8c2
ryoo14 <anana12185@gmail.com>
2020-11-08 16:59:05 +0900

    read subreddit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//use config::Config;
+//use roux::Reddit;
+use roux::Subreddit;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let subreddit = Subreddit::new("battlestations");
+    let top = subreddit.top(25, None).await;
+    println!("{:?}", top.unwrap());
 }
