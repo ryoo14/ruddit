dbad5bd79bee69e570201dc7d2d2f09f3f4268e0
ryoo14 <anana12185@gmail.com>
2020-11-08 19:52:43 +0900

    show title and url sample

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-//use config::Config;
-//use roux::Reddit;
 use roux::Subreddit;
 use tokio;
 
 #[tokio::main]
 async fn main() {
     let subreddit = Subreddit::new("battlestations");
-    let top = subreddit.top(25, None).await;
-    println!("{:?}", top.unwrap());
+    let top = subreddit.top(10, None).await.unwrap();
+    for i in top.data.children {
+        if let Some(v) = i.data.url {
+            println!("{} : {}", i.data.title, v);
+        }
+    }
 }
