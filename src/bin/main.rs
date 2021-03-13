d0bf8455665c308c07a1a11e885a4e10cbc123f6
ryoo14 <anana12185@gmail.com>
2021-03-13 20:12:41 +0900

    tui

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,16 @@
 use roux::{Me, Reddit, Subreddit, User};
 use ruddit::{setting::Setting, subscribe_data::RedditResponse};
+use std::io;
+use termion::raw::IntoRawMode;
 use tokio;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
 use ureq;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), io::Error> {
     // // display subreddit contents
     // let subreddit = Subreddit::new("battlestations");
     // let top = subreddit.top(10, None).await.unwrap();
@@ -45,9 +51,41 @@ async fn main() {
     if resp.ok() {
         let json = resp.into_json_deserialize::<RedditResponse>().unwrap();
         for i in json.data.children {
-            println!("{}", i.data.display_name);
+            //            println!("{}", i.data.display_name);
         }
     } else {
         println!("{}", String::from("NG!"));
     }
+
+    // build terminal
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    loop {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            let block = Block::default().title("Block 2").borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+        })?;
+
+        if let Event::Input(key) = events.next()? {
+            if key == Key::Char('q') {
+                break;
+            }
+        }
+    }
+    Ok(())
 }
